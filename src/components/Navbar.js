c90f7213ca6fc7c39259466235beb4b8ab76b161
oirinao <50031634+oirinao@users.noreply.github.com>
2022-04-28 01:27:17 +0300

    offer feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,45 +19,45 @@ const Navbar = props => {
 
 
   return (
-    <nav 
+    <nav
       id={props.id || ''}
-      className="navbar is-fresh is-transparent no-shadow" 
-      role="navigation" 
+      className="navbar is-fresh is-transparent no-shadow"
+      role="navigation"
       aria-label="main navigation">
       <div className="container">
         <div className="navbar-brand">
-          <Link 
+          <Link
             to="/"
             className="navbar-item">
-             <div className="title">Servicario</div>
+            <div className="title">Servicario</div>
           </Link>
 
           <a className="navbar-item is-hidden-desktop is-hidden-tablet">
-            <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{visibility: 'visible'}}>
+            <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{ visibility: 'visible' }}>
               <svg width="1000px" height="1000px">
-                  <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
-                  <path className="path2" d="M 300 500 L 700 500"></path>
-                  <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
+                <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
+                <path className="path2" d="M 300 500 L 700 500"></path>
+                <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
               </svg>
               <button id="menu-icon-trigger" className="menu-icon-trigger"></button>
             </div>
           </a>
 
           <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
         </div>
 
         <div id="navbar-menu" className="navbar-menu is-static">
           <div className="navbar-start">
             <a className="navbar-item is-hidden-mobile">
-              <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{visibility: 'visible'}}>
+              <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{ visibility: 'visible' }}>
                 <svg width="1000px" height="1000px">
-                    <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
-                    <path className="path2" d="M 300 500 L 700 500"></path>
-                    <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
+                  <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
+                  <path className="path2" d="M 300 500 L 700 500"></path>
+                  <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
                 </svg>
                 <button id="menu-icon-trigger" className="menu-icon-trigger"></button>
               </div>
@@ -65,67 +65,77 @@ const Navbar = props => {
           </div>
 
           <div className="navbar-end">
-            { user &&
+            {user &&
               <div className="navbar-item is-secondary user-welcome">
                 {`Hi ${user.fullName}`}
               </div>
-            } 
-            <Link 
-              to="/" 
+            }
+            <Link
+              to="/"
               className="navbar-item is-secondary">
-                Home
+              Home
             </Link>
-            <Link 
-              to="/services" 
+            <Link
+              to="/services"
               className="navbar-item is-secondary">
-                Services
+              Services
             </Link>
-            <Link 
-              to="/faq" 
+            <Link
+              to="/faq"
               className="navbar-item is-secondary">
-                Faq
+              Faq
             </Link>
-            { isAuth &&
+            {isAuth &&
               <div className="navbar-item has-dropdown is-hoverable">
                 <a className="navbar-link">
-                    Manage
+                  Manage
                 </a>
                 <div className="navbar-dropdown">
-                  <Link 
+                  <Link
                     to="/services/new"
                     className="navbar-item">
-                      Create Service
+                    Create Service
                   </Link>
-                  <Link 
+                  <Link
                     to="/services/me"
                     className="navbar-item">
-                      Your Services
+                    Your Services
+                  </Link>
+                  <Link
+                    to="/offers/sent"
+                    className="navbar-item">
+                    Sent Offers
+                  </Link>
+                  <Link
+                    to="/offers/received"
+                    className="navbar-item">
+                    Received Offers
                   </Link>
                 </div>
               </div>
             }
-            { !isAuth &&
+            {!isAuth &&
               <React.Fragment>
                 <Link
                   to="/login"
                   className="navbar-item is-secondary modal-trigger" data-modal="auth-modal">
-                    Login
+                  Login
                 </Link>
-                <Link 
+                <Link
                   to="/register"
                   className="navbar-item">
                   <span className="button signup-button rounded secondary-btn raised">
-                      Register
+                    Register
                   </span>
                 </Link>
               </React.Fragment>
             }
-            { isAuth &&
-              <div 
+            {isAuth &&
+              <div
                 onClick={logout}
                 className="navbar-item">
                 <span className="button signup-button is-danger rounded raised">
-                    Logout
+                  Logout
                 </span>
               </div>
             }
